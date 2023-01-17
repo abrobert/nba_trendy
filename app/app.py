40bd775d5184940f85496e8294eb99cf5b8e69a9
abroberts14 <abroberts14@gmail.com>
2023-01-17 14:38:32 -0500

    fix

--- app/app.py
+++ app/app.py
@@ -22,7 +22,7 @@ def create_app() -> Flask:
             
             app.config.from_object("config")
             db = SQLA(app)
-            from views import CustomPlayModelView, PlayModelView, PlayChartView, PlayChartView2, MyView
+            #from views import CustomPlayModelView, PlayModelView, PlayChartView, PlayChartView2, MyView
 
             db.init_app(app)
 
@@ -30,30 +30,31 @@ def create_app() -> Flask:
             appbuilder = AppBuilder(app, db.session, base_template='custom_base.html')
             db.create_all()
 
+            from views import PlayModelView
 
-            from views import CustomPlayModelView, PlayModelView, PlayChartView, PlayChartView2, MyView
+            #from views import CustomPlayModelView, PlayModelView, PlayChartView, PlayChartView2, MyView
             appbuilder.add_view(
                 PlayModelView, "List Plays", icon="fa-envelope", category="Plays"
             )
-            appbuilder.add_view(
-                CustomPlayModelView, "List Custom Plays", icon="fa-envelope", category="Plays"
-            )
-            #appbuilder.add_view(CustomPlayModelView, "List Custom Plays", href=CustomPlayModelView.endpoint+"/table/", icon="fa-envelope", category="Plays")
-            appbuilder.add_view(
-                PlayChartView,
-                "Show Play Chart",
-                icon="fa-dashboard",
-                category="Statistics",
-            )
-            appbuilder.add_view(
-                PlayChartView2,
-                "Show Play Chart2",
-                icon="fa-dashboard",
-                category="Statistics",
-            )
-            appbuilder.add_view(MyView, "Method1", category='My View')
-            appbuilder.add_link("Method2", href='/myview/method2/john', category='My View')
-            appbuilder.add_link("Method3", href='/myview/method3/john', category='My View')
+            # appbuilder.add_view(
+            #     CustomPlayModelView, "List Custom Plays", icon="fa-envelope", category="Plays"
+            # )
+            # #appbuilder.add_view(CustomPlayModelView, "List Custom Plays", href=CustomPlayModelView.endpoint+"/table/", icon="fa-envelope", category="Plays")
+            # appbuilder.add_view(
+            #     PlayChartView,
+            #     "Show Play Chart",
+            #     icon="fa-dashboard",
+            #     category="Statistics",
+            # )
+            # appbuilder.add_view(
+            #     PlayChartView2,
+            #     "Show Play Chart2",
+            #     icon="fa-dashboard",
+            #     category="Statistics",
+            # )
+            # appbuilder.add_view(MyView, "Method1", category='My View')
+            # appbuilder.add_link("Method2", href='/myview/method2/john', category='My View')
+            # appbuilder.add_link("Method3", href='/myview/method3/john', category='My View')
 
         return app
     except Exception as ex:
